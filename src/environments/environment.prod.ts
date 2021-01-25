586a27c140e3bf62ebf8e69f624b7bee7debec51
arpemar98 <arpemar98@hotmail.com>
2021-01-25 17:08:39 -0600

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCIHbe1P_MllG7p-Yl0lBt__N6efjB2r6I",
+    authDomain: "puntodeventa-arpemar.firebaseapp.com",
+    projectId: "puntodeventa-arpemar",
+    storageBucket: "puntodeventa-arpemar.appspot.com",
+    messagingSenderId: "259358379825",
+    appId: "1:259358379825:web:5ca6cc4098de858ef02a10",
+    measurementId: "G-XMP4K0SQTL"
+  }
 };
